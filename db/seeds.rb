44fb42f39e3167724d780e7cbfd58669d6c13491
Francois Nguyen <fnguyen90@gmail.com>
2020-08-13 15:27:37 +0200

    super seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'open-uri'
-require 'json'
+require "open-uri"
+require "json"
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 read = open(url).read
 json_parsed = JSON.parse(read)
 json_parsed["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient.values.first)
+end
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+read = open(url).read
+json_parsed = JSON.parse(read)
+json_parsed["drinks"].each do |cocktail|
+  cocktail_name = cocktail["strDrink"]
+  cocktail_created = Cocktail.create(name: cocktail_name)
+  ingredients_hash = cocktail.select { |k, _| k.include? "strIngredient" }
+  ingredients_array = ingredients_hash.values.reject { |e| e.to_s.empty? }
+  measures_hash = cocktail.select { |k, _| k.include? "strMeasure" }
+  measures_array = measures_hash.values.reject { |e| e.to_s.empty? }
+  i = 0
+  ingredients_array.each do |ingredient|
+    ingredient = Ingredient.where(name: ingredient).take
+    dose_desc = measures_array[i]
+    i += 1
+    if ing
+      Dose.create(description: dose_desc, cocktail_id: cocktail_created.id, ingredient_id: ingredient.id)
+    end
+  end
 end
